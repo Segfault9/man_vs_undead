92131f8a664a77ad4a8a4353bedffb1c36ffc34d
AlexiaTran <alexia.tran98@gmail.com>
2021-01-24 21:08:15 +0100

    création fenetre graphique

--- main.cc
+++ main.cc
@@ -9,6 +9,7 @@
 #include "Arene.hh"
 #include "Entite.hh"
 #include "Jeu.hh"
+#include <SFML/Graphics.hpp>
 
 
 int main(){
@@ -17,8 +18,28 @@ int main(){
 
     //std::cout << a.toString() << endl;
 
-    j.spawner(5);
-    j.spawn_joueur(j.getJoueur());
+    //j.spawner(5);
+   // j.spawn_joueur(j.getJoueur());
+
+
+    sf::RenderWindow window(sf::VideoMode(1500, 900), "Man vs Undead");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
 
     return 0;
 }
