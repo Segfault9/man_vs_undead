4b21404f6f6c0903c3a3a058cee3b20d0170e699
AlexiaTran <58553724+AlexiaTran@users.noreply.github.com>
2021-01-25 07:45:11 +0100

    reso conflit

--- Zombie.cc
+++ Zombie.cc
@@ -2,12 +2,20 @@
 #include <cstdlib>
 #include <time.h>
 #include <stdlib.h>
+#include <ctime>
+#include <chrono>
+#include <random>
 
 using namespace std;
 #define PV_MAX 100
 
+// obtain a seed from the system clock:
+std::default_random_engine Zombie::_generator(std::chrono::system_clock::now().time_since_epoch().count());
+std::uniform_int_distribution<int> Zombie::_distribution(33,94);
+
 int Zombie::cpt = 0;
 
+//constructeur
 Zombie::Zombie(string name, Position pos): Entite(name, pos){
   	PV=PV_MAX;
   	speed=1;
@@ -17,6 +25,7 @@ Zombie::Zombie(string name, Position pos): Entite(name, pos){
   			
 }
 
+//constructeur 
 Zombie::Zombie(string name): Entite(name){
   	PV=PV_MAX;
   	speed=1;
@@ -37,6 +46,7 @@ Zombie::Zombie():Entite("Zombie " + to_string(cpt)){
 
 }
 
+//destructeur
 Zombie::~Zombie(){
 	--cpt;
 }
@@ -64,6 +74,7 @@ void Zombie::setPV(int pv){
 	PV=pv;
 }
 
+// 
 int Zombie::mort(){
 
 	if(PV <= 0){
@@ -73,6 +84,71 @@ int Zombie::mort(){
 	return 0;
 }
 
+//mouvement du zombie 
+
+void Zombie::move_up(){//0
+	if(pos.y > 0+speed)//si en faisant -speed je suis toujours dans le fenetre je me deplace
+		pos.y -= speed;
+  	else //sinon je reste la ou je suis
+    	pos.y = pos.y;
+   
+}
+
+void Zombie::move_down(){ //1
+	if(pos.y <= 899-speed)//si en faisant +speed je suis toujours dans le fenetre je me deplace
+		pos.y += speed;
+  	else //sinon je reste la ou je suis
+    	pos.y = pos.y;
+  pos.y += speed; 
+}
+
+void Zombie::move_left(){//2
+ 	if(pos.x > 0+speed)//si en faisant -speed je suis toujours dans le fenetre je me deplace
+		pos.x -= speed;
+  	else //sinon je reste la ou je suis
+    	pos.x = pos.x; 
+}
+
+void Zombie::move_right(){//3
+	if(pos.x <= 1124-speed)//si en faisant +speed je suis toujours dans le fenetre je me deplace
+    	pos.x += speed; 
+  	else //sinon je reste la ou je suis
+    	pos.x = pos.x;
+}
+
+void Zombie::move(vector<Zombie> z){
+  _distribution(_generator);
+  
+
+  for(unsigned int i=0; i<z.size(); i++){ //move aléatoire pour chaque zombie
+    int rand_move = rand()%4; //0 to 3 
+
+    switch(rand_move){
+    case 0: // up
+      z[i].move_up();
+      break;
+
+    case 1: // down 
+      z[i].move_down();
+      break;
+
+    case 2: //left
+      z[i].move_left();
+      break;
+
+    case 3://right
+      z[i].move_right();
+      break;
+
+    default: 
+      break;
+
+    }  
+  }
+}
+
+
+/*
 void Zombie::move(int speed){
 	// Déplacement aléatoire
 	int val = random() % 4;
@@ -100,3 +176,4 @@ void Zombie::move(int speed){
 			break;
 	}
 }
+*/
